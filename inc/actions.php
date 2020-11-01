062a672016bd69602311bd3a90826039a4caf0ea
faissaloux <fwahabali@gmail.com>
2020-11-01 16:30:23 +0100

    switch actions section to rtl

--- inc/actions.php
+++ inc/actions.php
@@ -3,7 +3,7 @@
         <div class="d-flex">
             <div class="d-flex col-2 justify-content-center align-items-center">
                 <div>
-                    <span class="col-8">Number of actions:</span>
+                    <span class="col-8">عدد العمليات:</span>
                     <span class="col-2 actions-number">1</span>
                 </div>
             </div>
@@ -11,45 +11,45 @@
                 <div class="col-6">
                     <div class="d-flex justify-content-around">
                         <span class="action d-flex align-items-center">
-                            <div class="d-flex">
-                                <i class="mdi mdi-upload mr-2"></i>
-                                <a  class="table-top-btn col"
-                                            style="color: white"
-                                            data-toggle="modal"
-                                            data-target="#exportModalCenter">Export</a>
-                            </div>
+                            <a  class="table-top-btn"
+                                style="color: white"
+                                data-toggle="modal"
+                                data-target="#exportModalCenter">
+                                <i class="mdi mdi-upload ml-2"></i>
+                                <span>تصدير</span>
+                            </a>
                         </span>
                         <span class="action d-flex align-items-center">
-                            <div class="d-flex">
-                                <i class="mdi mdi-clipboard-check mr-2"></i>
-                                <span>assign</span>
-                            </div>
+                            <a>
+                                <i class="mdi mdi-clipboard-check ml-2"></i>
+                                <span>تعيين</span>
+                            </a>
                         </span>
                         <span class="dropdown action d-flex align-items-center">
-                            <span   class="dropdown-toggle d-flex align-items-center"
+                            <a   class="dropdown-toggle d-flex align-items-center"
                                     id="dropdownMenuButton"
                                     data-toggle="dropdown"
                                     aria-haspopup="true"
                                     aria-expanded="false">
-                                <i class="mdi mdi-backup-restore mr-2"></i>
-                                <span type="button"> Restore </span>
-                            </span>
-                            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                <i class="mdi mdi-backup-restore ml-2"></i>
+                                <span type="button" class="ml-2">استعادة</span>
+                            </a>
+                            <div class="dropdown-menu tx-right" aria-labelledby="dropdownMenuButton">
                                 <a class="dropdown-item" href="#">
-                                    <i class="mdi mdi-content-duplicate mr-2"></i>
-                                    Restore from duplicate
+                                    <i class="mdi mdi-content-duplicate ml-2"></i>
+                                    استعادة من مكرر
                                 </a>
                                 <a class="dropdown-item" href="#">
-                                    <i class="mdi mdi-delete-variant mr-2"></i>
-                                    Restore from trash
+                                    <i class="mdi mdi-delete-variant ml-2"></i>
+                                    استعادة من سلة المهملات
                                 </a>
                             </div>
                         </span>
                         <span class="action action-delete d-flex align-items-center">
-                            <div class="d-flex">
-                                <i class="mdi mdi-delete mr-2"></i>
-                                <span>Delete</span>
-                            </div>
+                            <a class="d-flex">
+                                <i class="mdi mdi-delete ml-2"></i>
+                                <span>حذف</span>
+                            </a>
                         </span>
                     </div>
                 </div>
