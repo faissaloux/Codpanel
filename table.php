2aba6b2f9309144f3c32f2e379724366982bdbcb
faissaloux <fwahabali@gmail.com>
2020-11-03 17:06:57 +0100

    separate actions css

--- table.php
+++ table.php
@@ -13,6 +13,7 @@
         <!-- Main CSS -->
         <link type="text/css" rel="stylesheet" href="assets/css/all.css"/>
         <link type="text/css" rel="stylesheet" href="assets/css/custom.css"/>
+        <link type="text/css" rel="stylesheet" href="assets/css/actions.css"/>
         <!-- Favicon -->	
         <link rel="icon" href="assets/images/favicon.ico" type="image/x-icon">
             
